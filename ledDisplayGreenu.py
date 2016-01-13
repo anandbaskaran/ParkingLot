6d7178f8de1f936aae2d0cd9f9a7476357adc0db
root <root@iottue.(none)>
2016-01-13 16:34:54 +0000

    first commit

---
+++ ledDisplayGreenu.py
@@ -0,0 +1,8 @@
+from sense_hat import SenseHat 
+sense = SenseHat()
+# set up the colours (white, green, red, empty)
+green = [0,255,0]
+orange = [255, 128, 0]
+red = [255,0,0]
+sense.show_letter("F",text_colour=green)
+
